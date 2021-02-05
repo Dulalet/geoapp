75c9e089535083f692b82cd41e29c777ac6d7ad6
dulalet <daulet.abduali@gmail.com>
2021-02-05 17:02:56 +0600

    swagger added

--- geoapp/urls.py
+++ geoapp/urls.py
@@ -16,17 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
-from geo import views
-
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-
+schema_view = get_swagger_view(title='geoapp')
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('', schema_view),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('geo/', include('geo.urls'))
