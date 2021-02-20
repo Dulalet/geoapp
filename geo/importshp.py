147fb85917c3ad2c2f8f81dd920ac71808b2a1cb
dulalet <daulet.abduali@gmail.com>
2021-02-21 02:20:01 +0600

    import any shp file to database. next step: add editing functionality

--- geo/importshp.py
+++ geo/importshp.py
@@ -7,44 +7,6 @@ from geo.models import Layer
 
 gdf = gpd.read_file('/home/daulet/Desktop/WGS2/Hydrography.shp')
 geomList = gdf.geometry.to_list()
-# l.save()
-# tempStr = ""
-# geomArr = []
-#
-# for i in geomString:
-#     if i == 'P':
-#         for j in range(geomString):
-#
-#         while i != "\n":
-#             tempStr = tempStr + i
-#             print(tempStr)
-#             iter(i)
-#         geomArr.extend(tempStr)
-#         tempStr = ''
-# print(geomArr)
-#
-#
-# for i in range(geomString):
-#     if geomString[i] == 'P':
-#         j = i
-#         while j != '\n':
-#             tempStr = tempStr + i
-#             print(tempStr)
-#             j+=1
-#         geomArr.extend(tempStr)
-#         tempStr = ''
-# print(geomArr)
-#
-# arr = geomString.split('\n')
-# for i in range(len(arr)):
-#     temp=arr[i][::]
-#
-# for i in range(1, 1000):
-#     print(arr[0][i])
-
-# print(geomList[0].wkt)
-# a = geomList[0].wkt
-# poly = GEOSGeometry(a, srid=3857)
 
 for i in range(len(geomList)):
     geomList[i] = geomList[i].wkt
