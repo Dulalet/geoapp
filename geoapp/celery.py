04d54d80c6a66ad95397dd6e07d1a32d472bb91c
Dulalet <daulet.abduali@gmail.com>
2021-04-16 10:08:21 +0000

    celery updated

--- geoapp/celery.py
+++ geoapp/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoapp.settings')
 
 app = Celery('geoapp')
-app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.broker_url = 'redis://redis:6379/0'
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
