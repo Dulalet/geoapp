358e8327e0bc06e4f1b8368ef2e7c6a7b314ef69
dulalet <daulet.abduali@gmail.com>
2021-04-20 18:43:06 +0600

    settings fixes

--- geoapp/settings.py
+++ geoapp/settings.py
@@ -156,8 +156,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'geoapp/media')
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
